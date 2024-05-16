8ce18e67dea0a95f8dcbd6a85a560fb02f4c71f7
isayhc <isayahculbertson@gmail.com>
2024-05-16 09:38:14 -0400

    added english and french tts

--- src/seamless/speech_to_text.py
+++ src/seamless/speech_to_text.py
@@ -22,14 +22,6 @@ async def fon_speech_to_english(filename):
         async with session.post(API_URL, headers=headers, data=data) as response:
             return await response.json()
         
-# async def fon_text_to_french(text:str):
-#     API_URL = "https://api-inference.huggingface.co/models/masakhane/m2m100_418M_fon_fr_rel_news"
-#     headers = {"Authorization": f"Bearer {os.getenv('HUGGINGFACEHUB_API_TOKEN')}"}
-#     async with aiohttp.ClientSession() as session:
-#         # with open(filename, "rb") as f:
-#         #     data = f.read()
-#         async with session.post(API_URL, headers=headers, data=text) as response:
-#             return await response.json()
 
 async def fon_text_to_french(text: str):
     API_URL = "https://api-inference.huggingface.co/models/masakhane/m2m100_418M_fon_fr_rel_news"
@@ -40,6 +32,36 @@ async def fon_text_to_french(text: str):
             async with session.post(API_URL, headers=headers, data=text) as response:
                 result = await response.json()
                 if 'error' not in result or result.get('error') != 'Model masakhane/m2m100_418M_fon_fr_rel_news is currently loading':
+                    break  # Exit loop if response is not in loading state or does not contain error
+                await asyncio.sleep(5)  # Wait for 5 seconds before checking again
+
+    return result
+
+async def english_tts(file_name: str):
+    API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v3"
+    headers = {"Authorization": f"Bearer {os.getenv('HUGGINGFACEHUB_API_TOKEN')}"}
+    with open(file_name, "rb") as f:
+        data = f.read()
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.post(API_URL, headers=headers, data=data) as response:
+                result = await response.json()
+                if 'error' not in result:
+                    break  # Exit loop if response is not in loading state or does not contain error
+                await asyncio.sleep(5)  # Wait for 5 seconds before checking again
+
+    return result
+
+async def french_tts(file_name: str):
+    API_URL = "https://api-inference.huggingface.co/models/BrunoHays/whisper-large-v3-french-illuin"
+    headers = {"Authorization": f"Bearer {os.getenv('HUGGINGFACEHUB_API_TOKEN')}"}
+    with open(file_name, "rb") as f:
+        data = f.read()
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.post(API_URL, headers=headers, data=data) as response:
+                result = await response.json()
+                if 'error' not in result:
                     break  # Exit loop if response is not in loading state or does not contain error
                 await asyncio.sleep(5)  # Wait for 5 seconds before checking again
 
@@ -72,16 +94,17 @@ async def english_text_to_fon_speech(text: str, save_file_name:str) -> bytes:
 
 
 async def main():
-    output = await fon_speech_to_text("/home/isayahc/projects/FanTonic/edba837d-d79b-4945-8159-ecaa3677b12e.flac")
-    print(output)
+    # output = await fon_speech_to_text("/home/isayahc/projects/FanTonic/edba837d-d79b-4945-8159-ecaa3677b12e.flac")
+    # print(output)
     
     # alt_output = await fon_speech_to_english("/home/isayahc/projects/FanTonic/edba837d-d79b-4945-8159-ecaa3677b12e.flac")
     # print(alt_output)
     
-    alt_alt_output = await fon_text_to_french(output['text'])
-    print(alt_alt_output)
+    # alt_alt_output = await fon_text_to_french(output['text'])
+    # print(alt_alt_output)
     
-    dd  = await english_text_to_fon_speech("Hi my name is Isayah am i am trying a thing to live life","sample.wav")
+    # dd  = await english_text_to_fon_speech("Hi my name is Isayah am i am trying a thing to live life","sample.wav")
+    data = await english_tts("/home/isayahc/projects/FanTonic/Alina_Eng.wav")
     x=0
 
 if __name__ == '__main__':
